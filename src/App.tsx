aa39266fd366d6e2d9c362b933e159b92e4df3c9
jjung-in <4292in@naver.com>
2025-02-21 04:58:28 +0900

    Feat: TanStack Query 설정 추가
    - react-query 및 react-query-devtools 패키지 설치
    - QueryClient 및 QueryClientProvider를 사용하여 전역 상태 관리 설정
    - ReactQueryDevtools 추가하여 개발 도구 활성화

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,7 +28,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
