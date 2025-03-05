ef3dd635a06afab90c063c7e5c3f23d62b44cc5f
choiyh0617 <6l0mrc00057@gmail.com>
2025-03-05 12:42:22 +0900

    Style : 문서 서식 변경

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,15 +1,86 @@
-import { SignUpProps, LogInProps} from "@_types/auth.type"
-import { PostSignUp, PostLogIn, EmailDuplicateCheck, GetTemporaryPassword, SendAuthenticationNumber, CheckSendAuthenticationNumber } from "@_api/auth"
-import { useQuery } from '@tanstack/react-query';
+import { SignUpProps, LogInProps } from '@_types/auth.type';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import {
+  PostSignUp,
+  PostLogIn,
+  EmailDuplicateCheck,
+  GetTemporaryPassword,
+  SendAuthNumber,
+  CheckAuthNumber,
+} from '@_api/auth';
 
-export const UseLogIn = (LogInData : LogInProps) => {
-    const { data, error } = useQuery<LogInProps>({
-        queryKey: ['logIn', LogInData],
-        queryFn: () => PostLogIn(LogInData),
-        enabled : !!logInData.username && !!logInData.password,
-        onError: (error) => {
-            console.error(error);
-        }
-    });
-    return { data, error };
+export const useLogIn = (LogInData: LogInProps) => {
+  const { data } = useQuery<LogInProps[]>({
+    queryKey: ['logIn', LogInData],
+    queryFn: () => PostLogIn(LogInData),
+    enabled: !!logInData.username && !!logInData.password,
+    onError: (error) => {
+      console.error('LogIn Error', error);
+    },
+  });
+  return { data };
+};
+
+export const useSignUp = (SignUpData: SignUpProps) => {
+  const { data } = useMutation<SignUpProps[]>({
+    mutationKey: ['signUp', SignUpData],
+    mutationFn: () => PostSignUp(SignUpData),
+    onSuccess: (data) => {
+      console.log('SignUp Success', data);
+    },
+    onError: (error) => {
+      console.error('SignUp Error', error);
+    },
+  });
+  return { data };
+};
+
+export const useEmailDuplicateCheck = (email: string) => {
+  const { data } = useQuery<boolean>({
+    queryKey: ['login', email],
+    queryFn: () => EmailDuplicateCheck(email),
+    enabled: !!Email,
+    onError: (error) => {
+      console.error('EmailDuplicate Error', error);
+    },
+  });
+  if (data === true) {
+    console.log('이메일이 중복되지 않습니다.');
+  } else {
+    console.log('이미 존재하는 이메일입니다.');
+  }
+  return { data };
+};
+
+export const useGetTemporaryPassword = (email: string) => {
+  const { data } = useQuery<string>({
+    queryKey: ['password', email],
+    queryFn: () => GetTemporaryPassword(email),
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+  return { data };
+};
+
+export const useSendAuthNumber = (email: string) => {
+  const { data } = useQuery<string[]>({
+    queryKey: ['number', email],
+    queryFn: () => SendAuthNumber(email),
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+  return { data };
+};
+
+export const useCheckAuthNumber = (email: string, number: string) => {
+  const { data } = useQuery<string[]>({
+    queryKey: ['number', { email, number }],
+    queryFn: () => CheckAuthNumber(email, number),
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+  return { data };
 };
