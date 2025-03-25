3b7f77c0e444facc12f660275f82be917228aaef
choiyh0617 <6l0mrc00057@gmail.com>
2025-03-25 18:10:41 +0900

    Feat : 이메일 중복 체크
    - 이메일 중복체크 추가
    - 변수명 수정

--- src/pages/Auth/SignUp/SignUp.tsx
+++ src/pages/Auth/SignUp/SignUp.tsx
@@ -1,104 +1,119 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { SignUpProps } from '@_types/auth.type';
-import { useSignUp } from '@_hooks/useAuth';
+import { useSignUp, useEmailDuplicateCheck } from '@_hooks/useAuth';
 
 const SignUp = () => {
   const [FormData, setFormData] = useState<SignUpProps>({
-    Name: '',
-    Email: '',
-    Password: '',
-    ConfirmPassword: '',
-    PhoneNumber: '',
+    name: '',
+    email: '',
+    password: '',
+    contact: '',
   });
 
-  const navigate = useNavigate();
-  const { data, mutate: signUp } = useSignUp(FormData);
+  const [mailActivate, setmailActivate] = useState(false)
+
+  const { mutate: signUp } = useSignUp();
+  const { data: isEmailAvailable } = useEmailDuplicateCheck(FormData.email);
 
   const HandleSignUp = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!FormData.Email || !FormData.Password || !FormData.ConfirmPassword || !FormData.PhoneNumber || !FormData.Name) {
+    if (!FormData.email || !FormData.password || !FormData.contact || !FormData.name) {
       console.log('모든 필드를 채워주세요.');
       return;
     }
-    if (FormData.Password !== FormData.ConfirmPassword) {
+    if (FormData.password !== FormData.confirmPassword) {
       console.log('비밀번호가 일치하지 않습니다.');
       return;
-    } else {
-      signUp({
-        Name: FormData.Name,
-        Email: FormData.Email,
-        Password: FormData.Password,
-        PhoneNumber: FormData.PhoneNumber,
-      });
     }
+    signUp(
+      {
+        name: FormData.name,
+        email: FormData.email,
+        password: FormData.password,
+        contact: FormData.contact,
+      },
+      {
+        onSuccess: () => {
+          console.log('회원가입 성공');
+          // navigate('/');
+        },
+      },
+    );
   };
-
+  
   useEffect(() => {
-    const HyphenPhoneNumber = FormData.PhoneNumber;
-
+    const HyphenPhoneNumber = FormData.contact;
+    
     if (HyphenPhoneNumber.length === 4) {
       setFormData((prevData) => ({
         ...prevData,
-        PhoneNumber: HyphenPhoneNumber.replace(/(\d{3})(\d{1})/, '$1-$2'),
+        contact: HyphenPhoneNumber.replace(/(\d{3})(\d{1})/, '$1-$2'),
       }));
     } else if (HyphenPhoneNumber.length === 9) {
       setFormData((prevData) => ({
         ...prevData,
-        PhoneNumber: HyphenPhoneNumber.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{1})/, '$1-$2-$3'),
+        contact: HyphenPhoneNumber.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{1})/, '$1-$2-$3'),
       }));
     }
-  }, [FormData.PhoneNumber]);
+  }, [FormData.contact]);
 
-  useEffect(() => {
-    if (data) {
-      console.log('회원가입 성공!', data);
-      navigate('../');
+  const emailChack = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (isEmailAvailable) {
+      console.log('이메일이 중복되지 않습니다.');
+      setmailActivate(true)
+    } else {
+      console.log('이미 존재하는 이메일입니다.',mailActivate);
     }
-  }, [data, navigate]);
+  };
 
   return (
     <div>
-      <form onSubmit={HandleSignUp}>
+      <form>
         <input
-          type="name"
+          type="text"
           placeholder="이름을 입력하세요"
-          value={FormData.Name}
-          onChange={(e) => setFormData({ ...FormData, Name: e.target.value })}
+          value={FormData.name}
+          onChange={(e) => setFormData({ ...FormData, name: e.target.value })}
         />
 
-        <input
-          type="email"
-          placeholder="이메일을 입력하세요"
-          value={FormData.Email}
-          onChange={(e) => setFormData({ ...FormData, Email: e.target.value })}
-        />
+        <span>
+          <input
+            type="email"
+            placeholder="이메일을 입력하세요"
+            value={FormData.email}
+            autoComplete="email"
+            onChange={(e) => setFormData({ ...FormData, email: e.target.value })}
+          />
+          <button onClick = {emailChack}>인증</button>
+        </span>
 
         <input
           type="password"
           placeholder="비밀번호를 입력하세요"
-          value={FormData.Password}
-          onChange={(e) => setFormData({ ...FormData, Password: e.target.value })}
+          value={FormData.password}
+          autoComplete="new-password"
+          onChange={(e) => setFormData({ ...FormData, password: e.target.value })}
         />
 
         <input
           type="password"
           placeholder="비밀번호를 다시 입력하세요"
-          value={FormData.ConfirmPassword}
-          onChange={(e) => setFormData({ ...FormData, ConfirmPassword: e.target.value })}
+          autoComplete="new-password"
+          value={FormData.confirmPassword}
+          onChange={(e) => setFormData({ ...FormData, confirmPassword: e.target.value })}
         />
 
         <input
           type="tel"
           placeholder="전화번호를 입력하세요"
-          value={FormData.PhoneNumber}
-          maxLength={13}
-          pattern="010-[0-9]{4}-[0-9]{4}"
-          onChange={(e) => setFormData({ ...FormData, PhoneNumber: e.target.value })}
+          value={FormData.contact}
+          onChange={(e) => setFormData({ ...FormData, contact: e.target.value })}
         />
 
-        <button type="submit">회원가입</button>
+        <button onClick = {HandleSignUp}>회원가입</button>
       </form>
     </div>
   );
