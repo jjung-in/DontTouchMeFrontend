5e62ceead5884bad54e04b940564db8cec8cdffd
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-01 21:43:59 +0900

    Feat : footer 디자인 추가
    - 공통 footer 디자인 추가
    - footer.ts, footer.tsx를 footer파일로 이동

---
+++ src/components/Layout/footer/Footer.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const BottomBar = styled.footer`
+border-top: 1px solid #E1E4ED;
+margin: 0 auto;
+width: 100%;
+height: 22px;
+padding: 24px 0px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+text-align: center;
+color: #B4B9C9;
+`;
