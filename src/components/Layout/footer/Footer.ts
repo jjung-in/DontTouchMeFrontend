8ec20fa5cc2390f82788c312c19dc7675f6d05f9
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-02 21:45:13 +0900

    Style : prettier 적용
    - Footer prettier적용
    - color hex 소문자로 수정

--- src/components/Layout/footer/Footer.ts
+++ src/components/Layout/footer/Footer.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 export const BottomBar = styled.footer`
-border-top: 1px solid #E1E4ED;
-margin: 0 auto;
-width: 100%;
-height: 22px;
-padding: 24px 0px;
+  border-top: 1px solid #e1e4ed;
+  margin: 0 auto;
+  width: 100%;
+  height: 22px;
+  padding: 24px 0px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 22px;
-text-align: center;
-color: #B4B9C9;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  color: #b4b9c9;
 `;
