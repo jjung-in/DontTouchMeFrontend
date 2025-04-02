c340dc5b4e253a1c66068ddc892acc484a8e8f8f
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-02 21:22:52 +0900

    Feat : button 컴포넌트 생성
    - button 컴포넌트 제작
    - Button 폴더 생성

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  position: absolute;
+  width: 311px;
+  height: 41px;
+  background: #3959a5;
+  border-radius: 5px;
+  border: none;
+
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 20px;
+  leading-trim: both;
+  text-edge: cap;
+  text-align: center;
+  color: #ffffff;
+`;
