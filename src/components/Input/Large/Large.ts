17faa283233ff126f3d9150064658cd472609b5c
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-03 21:53:51 +0900

    Design : Large input 디자인 변경
    - background -> background-color로 변경

--- src/components/Input/Large/Large.ts
+++ src/components/Input/Large/Large.ts
@@ -3,11 +3,8 @@ import styled from 'styled-components';
 export const Input = styled.input`
   width: 311px;
   height: 41px;
-  background: #ffffff;
+  background-color: #ffffff;
   border: 1px solid #d9d9d9;
   border-radius: 5px;
-  flex: none;
-  order: 1;
-  flex-grow: 0;
   font-family: 'Inter', sans-serif;
 `;
