96566907c79ea40c4719eb4af519eb54e6f930b0
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-15 23:49:12 +0900

    Feat : response 자동화 추가
    - 서버응답이 401이면, 토큰 재발급, 요청 재시도

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ReissueAccessToken } from '@_api/auth';
 
 export const instance = axios.create({
   baseURL: 'api/v1',
@@ -6,4 +7,24 @@ export const instance = axios.create({
     'Content-Type': 'application/json',
   },
   withCredentials: true,
-});
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newAccessToken = await ReissueAccessToken();
+        localStorage.setItem('accessToken', newAccessToken);
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+        return instance(originalRequest);
+      } catch (reissueError) {
+        console.error('토큰 재발급 실패', reissueError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
