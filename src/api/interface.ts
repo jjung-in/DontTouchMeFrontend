7154cdf911a7e81771a6a8aabee5778f7761d867
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-15 23:49:35 +0900

    Feat : request자동화 추가
    - accessToken을 헤더에 자동으로 붙임

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -7,6 +7,19 @@ export const instance = axios.create({
     'Content-Type': 'application/json',
   },
   withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
 instance.interceptors.response.use(
   (response) => response,
   async (error) => {
