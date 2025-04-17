951ce88de8610ef116acb7b4caad404680dc8d81
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-17 20:32:55 +0900

    Fix : color theme으로 변경
    - 컬러번호 -> theme에 있는 요소로 변경

--- src/components/Button/Small/Small.ts
+++ src/components/Button/Small/Small.ts
@@ -4,8 +4,8 @@ export const Button = styled.button`
 width: 145px;
 height: 40px;
 
-background: #FFFFFF;
-border: 1px solid #3959A5;
+background: ${({ theme }) => theme.color.primary[50]};
+border: 1px solid ${({ theme }) => theme.color.primary[500]};
 border-radius: 9px;
 
 font-family: 'Inter';
@@ -13,7 +13,5 @@ font-style: normal;
 font-weight: 500;
 font-size: 15px;
 
-color: #000000;
-
-
+color: ${({ theme }) => theme.text.black};
 `;
