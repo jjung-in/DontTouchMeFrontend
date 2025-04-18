ce6ed8e66866266f0058b7263972d4b03a720a5d
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-18 19:11:47 +0900

    Feat : Login 디자인 구현
    - 로그인 사용자 인터페이스 구현

---
+++ src/pages/Auth/LogIn/LogIn.styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { MainStyle } from '@_styles/common';
+import { FieldContainerStyle, FieldLabelStyle } from '@_styles/event';
+
+export const Main = styled.main`
+  ${MainStyle}
+  background-color: ${({ theme }) => theme.color.primary[200]};
+`;
+
+export const LogInForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 75px;
+  width: 900px;
+  padding: 100px 100px 50px 210px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.primary[50]};
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+`;
+
+export const FieldArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const FieldContainer = styled(FieldContainerStyle)`
+  padding-right: 110px;
+`;
+
+export const FieldLabel = styled(FieldLabelStyle)``;
+
+export const ButtonArea = styled.div`
+  padding-right: 110px;
+`;
+
+export const SignUpLinkArea = styled.div`
+  padding-right: 110px;
+  display: flex;
+  gap: 10px;
+  font-size: 14px;
+
+  a {
+    color: ${({ theme }) => theme.color.primary[400]};
+    text-decoration: underline;
+  }
+`;
