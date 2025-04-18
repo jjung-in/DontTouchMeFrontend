64eee6c675a704a55dff9f46b79bfbae0a0453c8
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-18 19:12:14 +0900

    Feat : 로그인 스타일 적용
    - 로그인 사용자 인터페이스 적용

--- src/pages/Auth/LogIn/LogIn.tsx
+++ src/pages/Auth/LogIn/LogIn.tsx
@@ -1,37 +1,68 @@
 import { useLogInFlow } from '@_hooks/useAuth';
 import { Link } from 'react-router-dom';
+import * as S from './LogIn.styles';
+import PageTitle from '@_components/Common/PageTitle/PageTitle';
+import Input from '@_components/Common/Input/Input';
+import Button from '@_components/Common/Button/Button';
+import { ErrorTextStyle } from '@_styles/event';
+
+const LOGIN_TITLE = {
+  title: '로그인',
+  highlight: '로그인',
+  subtitle: '이메일과 비밀번호를 입력해주세요',
+};
 
 const LogIn = () => {
-  const { formValues, handleChange, handleLogIn, isPending } = useLogInFlow();
+  const { formValues, formErrors, handleChange, handleLogIn, isPending } = useLogInFlow();
 
   return (
-    <main>
-      <form onSubmit={handleLogIn}>
-        <input
-          type="email"
-          name="email"
-          placeholder="이메일을 입력해주세요"
-          value={formValues.email}
-          onChange={handleChange}
-          required
-        />
+    <S.Main>
+      <PageTitle {...LOGIN_TITLE} />
+      <S.LogInForm onSubmit={handleLogIn}>
+        <S.FieldArea>
+          <S.FieldContainer>
+            <S.FieldLabel>이메일</S.FieldLabel>
+            <Input
+              type="email"
+              name="email"
+              value={formValues.email}
+              onChange={handleChange}
+              placeholder="이메일을 입력해주세요"
+              state={formErrors.email ? 'error' : 'default'}
+              autoComplete="off"
+              fullWidth
+            />
+            {formErrors.email && <ErrorTextStyle>{formErrors.email}</ErrorTextStyle>}
+          </S.FieldContainer>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="비밀번호를 입력해주세요"
-          value={formValues.password}
-          onChange={handleChange}
-          required
-        />
+          <S.FieldContainer>
+            <S.FieldLabel>비밀번호</S.FieldLabel>
+            <Input
+              type="password"
+              name="password"
+              value={formValues.password}
+              onChange={handleChange}
+              placeholder="비밀번호를 입력해주세요"
+              state={formErrors.password ? 'error' : 'default'}
+              autoComplete="current-password"
+              fullWidth
+            />
+            {formErrors.password && <ErrorTextStyle>{formErrors.password}</ErrorTextStyle>}
+          </S.FieldContainer>
+        </S.FieldArea>
 
-        <button type="submit" disabled={isPending}>
-          로그인
-        </button>
+        <S.ButtonArea>
+          <Button type="submit" variant="primary" fontWeight="semibold" fullWidth disabled={isPending}>
+            로그인
+          </Button>
+        </S.ButtonArea>
 
-        <Link to="/signup">회원가입</Link>
-      </form>
-    </main>
+        <S.SignUpLinkArea>
+          <span>계정이 없으신가요?</span>
+          <Link to="/signup">회원가입</Link>
+        </S.SignUpLinkArea>
+      </S.LogInForm>
+    </S.Main>
   );
 };
 
