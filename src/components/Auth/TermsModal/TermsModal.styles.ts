1013590ac014782705ef1494963d2ae627202f89
choiyh0617 <6l0mrc00057@gmail.com>
2025-04-26 16:19:47 +0900

    Feat : 모달 스타일

--- src/components/Auth/TermsModal/TermsModal.styles.ts
+++ src/components/Auth/TermsModal/TermsModal.styles.ts
@@ -17,19 +17,20 @@ export const ModalBox = styled.div`
   width: 1400px;
   height: 800px;
   background-color: #ffffff;
-  padding: 3.75rem;
+  padding: 3rem;
   border-radius: 12px;
   overflow-y: auto;
   position: relative;
 `;
 
-export const CloseButton = styled.img`
-  position: absolute;
-  top: 38px;
-  right: 38px;
+export const CloseImg = styled.img`
+  position: fixed;
+  top: calc(50% - 400px + 38px);
+  left: calc(50% + 700px - 76px);
   width: 33px;
   height: 33px;
   cursor: pointer;
+  z-index: 100;
 `;
 
 export const TextContainer = styled.div`
@@ -44,11 +45,43 @@ export const Title = styled.h1`
   margin-bottom: 0.3rem;
 `;
 
-export const Caption = styled.p<{ fontWeight?: 'bold' | 'normal'; color?: black | red }>`
-  fontsize: 'xl';
+export const Caption = styled.p<{ fontWeight?: 'bold' | 'normal'; color?: 'black' | 'red' }>`
+  font-size: ${({ theme }) => theme.fontSize['xl']};
   font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
   color: ${({ color, theme }) => (color ? theme.color.text[color] : theme.color.text.black)};
   margin-bottom: 0.3rem;
 `;
 
-export const 
+export const UnorderedTermsList = styled.ul`
+  list-style-type: disc;
+  padding-left: 1.5rem;
+`;
+
+export const OrderedTermsList = styled.ol`
+  list-style-type: decimal;
+  padding-left: 1.5rem;
+`;
+
+export const TermsItem = styled.li<{ fontWeight?: 'bold' | 'normal' }>`
+  font-size: ${({ theme }) => theme.fontSize['xl']};
+  font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
+  color: ${({ theme }) => theme.color.text.black};
+  margin-bottom: 0.3rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
+export const CloseButton = styled.button`
+  width: 145px;
+  height: 50px;
+  background-color: ${({ theme }) => theme.color.primary[500]};
+  color: ${({ theme }) => theme.color.text.white};
+  border-radius: 5px;
+  font-size: ${({ theme }) => theme.fontSize['xl']};
+  font-weight: ${({ theme }) => theme.fontWeight.normal};
+  cursor: pointer;
+`;
