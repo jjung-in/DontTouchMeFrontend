6bccd2fc2dffadea5f52826e41d3b37914a3101c
jjung-in <4292in@naver.com>
2025-05-08 23:16:47 +0900

    Fix: axios를 사용하여 이미지 업로드 요청 수정

--- src/api/image.ts
+++ src/api/image.ts
@@ -1,10 +1,11 @@
+import axios from 'axios';
 import { instance } from './instance';
 
 export const uploadImage = async (file: File) => {
   const { data } = await instance.get('/image/upload', { params: { fileName: file.name } });
   const { presignedUrl, fileUrl } = data;
 
-  await instance.put(presignedUrl, file, {
+  await axios.put(presignedUrl, file, {
     headers: { 'Content-Type': file.type },
   });
 
