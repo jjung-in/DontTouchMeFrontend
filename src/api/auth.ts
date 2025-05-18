cd38482f5a537cd1f624673cfdb3404297ffe501
choiyh0617 <6l0mrc00057@gmail.com>
2025-05-18 18:50:36 +0900

    Feat : 회원 정보 수정 api 추가
    - 회원 정보 수정 api 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -9,6 +9,8 @@ import {
   TSignUpResponse,
   TVerifyEmailCodeRequest,
   TVerifyEmailCodeResponse,
+  TProfileEditRequest,
+  TProfileEditResponse,
 } from '@_types/auth.type';
 import { parseAccessToken } from '@_utils/auth';
 import { AxiosError } from 'axios';
@@ -116,4 +118,9 @@ export const checkPassword = async (params: TCheckPasswordRequest): Promise<void
 
 export const withdrawMember = async (): Promise<void> => {
   await instance.delete('/member/withdraw');
+};
+
+export const editInformation = async (EditData: TProfileEditRequest): Promise<TProfileEditResponse> => {
+  const { data } = await instance.patch('/member/me',EditData);
+  return data;
 };
